47a49ac5b82a7df2a0ab1d1b0c16c7744e314996
Jerry Linder <glz67@mst.edu>
2017-12-02 20:39:39 -0600

    initial create-react-app app

---
+++ src/IncentiveTable.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import Table from 'react-bootstrap';
+
+class IncentiveTable extends Component {
+  render() {
+    return (
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>Assignment</th>
+            <th>Coursse</th>
+            <th>Due Date</th>
+            <th>Incentive</th>
+            <th>Currently Held Hostage</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </Table>
+    );
+  }
+}
+
+class IncentiveRow extends Component {
+  IncentiveRow(assignment,course,due_date,incentive,hostage_status)
+  {
+    this.assignment=assignment;
+    this.course=course;
+    this.due_date=due_date;
+    this.hostage_status=hostage_status;
+  }
+  render() {
+    return (
+          <tr>
+            <td>{this.assignment}</td>
+            <td>{this.course}</td>
+            <td>{this.due_date}</td>
+            <td>{this.incentive}</td>
+            <td>{this.hostage_status}</td>
+          </tr>
+    );
+  }
+}
+
+
+export default IncentiveTable;
+export { IncentiveRow };
